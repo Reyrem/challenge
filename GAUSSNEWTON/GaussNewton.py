e0983d535415aa677cae3f9977810a03e0b4d5fa
Chevallier <remi.chevallier2@gmail.com>
2020-12-09 18:25:35 +0100

    retour sur le code, 9-12 1645

--- GAUSSNEWTON/GaussNewton.py
+++ GAUSSNEWTON/GaussNewton.py
@@ -42,7 +42,7 @@ sensFile = 'round2_sensors.csv'
 
 #df = pd.read_csv(dataFile, engine='python', sep=",", error_bad_lines=False, quoting=csv.QUOTE_NONE)
 # df = pd.read_csv(dataFile, engine='python', sep=",", quoting=csv.QUOTE_NONE, error_bad_lines=False, encoding='utf-8')
-cols = pd.read_csv(file, nrows=1).columns
+cols = pd.read_csv(dataFile, nrows=1).columns
 
 df = pd.read_csv(dataFile, engine='python', sep=",",  error_bad_lines=False, usecols=cols, encoding='utf-8')
 
