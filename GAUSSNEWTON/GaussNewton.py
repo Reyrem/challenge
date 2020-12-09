e27a6768d822ac333bba7dd10ed673ce57b53abc
Chevallier <remi.chevallier2@gmail.com>
2020-12-09 17:06:51 +0100

    retour sur le code, 9-12 1645

--- GAUSSNEWTON/GaussNewton.py
+++ GAUSSNEWTON/GaussNewton.py
@@ -20,8 +20,8 @@ sensFile = 'round2_sensors.csv'
 
 
 #df = pd.read_csv(dataFile, engine='python', sep=",", error_bad_lines=False, quoting=csv.QUOTE_NONE)
-# df = pd.read_csv(dataFile, engine='python', sep=",", quoting=csv.QUOTE_NONE, error_bad_lines=False, encoding='utf-8')
-df = pd.read_csv(dataFile, engine='python', sep=",")
+df = pd.read_csv(dataFile, engine='python', sep=",", quoting=csv.QUOTE_NONE, error_bad_lines=False, encoding='utf-8')
+# df = pd.read_csv(dataFile, engine='python', sep=",")
 print(df.iloc[0])
 df_sens = pd.read_csv(sensFile, engine='python',  sep=",")
 sensorsTable = np.load('sensorsTableCorrected.npy')
