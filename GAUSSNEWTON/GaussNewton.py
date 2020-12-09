caa6102162617ac5d3eb1fb5d7c6481a92d6f4dd
Chevallier <remi.chevallier2@gmail.com>
2020-12-09 08:50:21 +0100

    retour sur le code, 9-12 0850

--- GAUSSNEWTON/GaussNewton.py
+++ GAUSSNEWTON/GaussNewton.py
@@ -120,20 +120,23 @@ def localise(indexTrame, verbose=True):
   result = so.least_squares(delta, x, jac='3-point', args=arguments, ftol = 10**(-8), xtol = 10**(-8), loss='soft_l1', x_scale=(0.01, 0.01, 10), bounds=bnds)
   
 
-  # try:
+  try:
       #if verbose:
         #print(x)
         #print(result.x)
         #print(result.success)
         #print(result.message)
-  print(indexTrame,"---------->", result.x)
-  df_sample.latitude.iloc[indexTrame] = x[0]
-  df_sample.longitude.iloc[indexTrame] = x[1]
-  df_sample.longitude.iloc[indexTrame] = x[2]
+    print(indexTrame,"---------->", result.x)
+    df_sample.latitude.iloc[indexTrame] = x[0]
+    df_sample.longitude.iloc[indexTrame] = x[1]
+    df_sample.longitude.iloc[indexTrame] = x[2]
 
   #T'as pas le droit de faire ça
-  # except IndexError : 
-  #     print("----------------------",indexTrame, df_sample.id.count(), "------------------------------------------")
+  except IndexError : 
+    print("----------------------",indexTrame, df_sample.id.count(), "------------------------------------------")
+    with open("frames_problematiques.txt",a) as fichier:
+      fichier.write(indexTrame, df_sample.id.count())
+
   #     lock.acquire()
   #     try : 
   #         exportData()
