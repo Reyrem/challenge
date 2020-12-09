0ec844371ce88cfbccacc23e48bb683655a6cd90
Chevallier <remi.chevallier2@gmail.com>
2020-12-09 18:00:07 +0100

    retour sur le code, 9-12 1645

--- GAUSSNEWTON/GaussNewton.py
+++ GAUSSNEWTON/GaussNewton.py
@@ -27,8 +27,9 @@ sensFile = 'round2_sensors.csv'
 #df = pd.read_csv(dataFile, engine='python', sep=",", error_bad_lines=False, quoting=csv.QUOTE_NONE)
 #df = pd.read_csv(dataFile, engine='python', sep=",", quoting=csv.QUOTE_NONE, error_bad_lines=False, encoding='utf-8')
 #df = pd.read_csv(dataFile, engine='python', sep=",")
-df = pd.read_csv(dataFile, sep=",",lineterminator='\n')
-
+try:
+  df = pd.read_csv(dataFile, sep=",",lineterminator='\n')
+except pd.errors.ParserError as detail : print(detail): 
 
 for i in range (1000) : print(df['measurements'].count())
 df_sens = pd.read_csv(sensFile, engine='python',  sep=",")
