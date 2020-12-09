0bebbbcb7cbb1976af091bef97e63566baddadef
Chevallier <remi.chevallier2@gmail.com>
2020-12-09 17:57:30 +0100

    retour sur le code, 9-12 1645

--- GAUSSNEWTON/GaussNewton.py
+++ GAUSSNEWTON/GaussNewton.py
@@ -22,12 +22,12 @@ sensFile = 'round2_sensors.csv'
 
 #df = pd.read_csv(dataFile, engine='python', sep=",", error_bad_lines=False, quoting=csv.QUOTE_NONE)
 # df = pd.read_csv(dataFile, engine='python', sep=",", quoting=csv.QUOTE_NONE, error_bad_lines=False, encoding='utf-8')
-try:
-  # df = pd.read_csv(dataFile, engine='python', sep=",",error_bad_lines=False)
-  #df = pd.read_csv(dataFile, engine='python', sep=",", error_bad_lines=False, quoting=csv.QUOTE_NONE)
-  #df = pd.read_csv(dataFile, engine='python', sep=",", quoting=csv.QUOTE_NONE, error_bad_lines=False, encoding='utf-8')
-  df = pd.read_csv(dataFile, engine='python', sep=",",lineterminator='\n')
-except (CParserError) as detail : print(detail)
+
+# df = pd.read_csv(dataFile, engine='python', sep=",",error_bad_lines=False)
+#df = pd.read_csv(dataFile, engine='python', sep=",", error_bad_lines=False, quoting=csv.QUOTE_NONE)
+#df = pd.read_csv(dataFile, engine='python', sep=",", quoting=csv.QUOTE_NONE, error_bad_lines=False, encoding='utf-8')
+df = pd.read_csv(dataFile, engine='python', sep=",",lineterminator='\n')
+
 
 
 for i in range (1000) : print(df['measurements'].count())
